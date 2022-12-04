aed661ed4e79a7544b00167620c303c9756a6670
TobyOkoye <tobyuokoye@gmail.com>
2022-12-04 12:57:41 -0600

    ALU is not full completed

--- ALU.go
+++ ALU.go
@@ -1,1 +1,33 @@
 package main
+
+var ALUResult uint16
+
+func ALUOpI(entry1 uint16, immediate uint16) {
+	switch opcode {
+	case "ADDI":
+		ALUResult = entry1 + immediate
+	case "SUBI":
+		ALUResult = entry1 - immediate
+	}
+}
+
+func ALUOp(entry1 uint16, entry2 uint16) {
+	switch opcode {
+	case "AND":
+		ALUResult = entry1 & entry2
+	case "ADD":
+		ALUResult = entry1 + entry2
+	case "ORR":
+		ALUResult = entry1 | entry2
+	case "SUB":
+		ALUResult = entry1 - entry2
+	case "LSR":
+		ALUResult = entry1 >> entry2
+	case "LSL":
+		ALUResult = entry1 << entry2
+	case "ASR":
+		ALUResult = entry1 >> entry2
+	case "EOR":
+		ALUResult = entry1 ^ entry2
+	}
+}
